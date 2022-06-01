707446d7f088f0011efe6a8892f479edf84b8e37
yosyuan <yosyuan.lee@gmail.com>
2022-06-01 20:03:20 +0800

    APPgame

--- app/src/main/java/tw/cgu/b0921246/app_game/game1.java
+++ app/src/main/java/tw/cgu/b0921246/app_game/game1.java
@@ -8,22 +8,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-public class game1 extends AppCompatActivity implements DialogInterface.OnClickListener {
+public class game1 extends AppCompatActivity implements DialogInterface.OnClickListener{
 
     TextView txv,point;
+    ImageView image;
     Button btn1,btn2,btn4;
-    int count=0;
+    public static int[] count={0,20,40,60,80,100};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game1);
-
         txv=findViewById(R.id.textView);
         point=findViewById(R.id.point);
+        image=findViewById(R.id.imageView23);
         btn1=findViewById(R.id.btn1);
         btn4=findViewById(R.id.btn4);
+
         new AlertDialog.Builder(this)
                 .setMessage("找出該關想要找出的物件，總共有5小關，每關可獲得20分\n準備好了嗎!")
                 .setCancelable(false)
@@ -43,34 +47,21 @@ public class game1 extends AppCompatActivity implements DialogInterface.OnClickL
             finish();
         }
     }
-
     public void ture(View view) {
-            Intent it1=new Intent(this,game2.class);
-            startActivity(it1);
-            point.setText("得分："+String.valueOf(count+=20));
-            return;
+        point.setText(String.valueOf(count[1]));
+        Intent it1= new Intent(this, game2.class);
+        String data=point.getText().toString();
+        it1.putExtra("分數",data);
+        startActivity(it1);
     }
     public void giveup(View view){
-        Intent it1=new Intent(this,game2.class);
-        startActivity(it1);
-        point.setText("得分：0");
+        Intent it2 = new Intent(this, game2.class);
+        //point.setText(count[0]);
+        //it2.putExtra("分數",point.getText());
+        startActivity(it2);
     }
     public void goback(View view){
-        Intent it1=new Intent(this,MainActivity.class);
+        Intent it1 = new Intent(this, MainActivity.class);
         startActivity(it1);
     }
-          /*  if (count==0){
-        AlertDialog.Builder bdr=new AlertDialog.Builder(this);
-        bdr.setMessage("找出唯一長角的人物\n按返回鍵繼續遊戲").setTitle("提示剩餘2次")
-                .setCancelable(true).show();
-        count++;
-    }
-
-        if (count==1){
-        AlertDialog.Builder bdr2=new AlertDialog.Builder(this);
-        bdr2.setMessage("位於第一列\n按返回鍵繼續遊戲").setTitle("提示剩餘1次")
-                .setCancelable(true).show();
-        count=count+2;
-    }*/
-
 }
